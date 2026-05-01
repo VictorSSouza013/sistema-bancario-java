e4cf7f55f6172318198f7f5cb388e77577b4d273
Victor Soares <victordesouza0013@gmail.com>
2026-05-01 18:08:46 -0300

    Adicionado validações de depósito e saque

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Locale;
 import java.util.Scanner;
 
 
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -11,37 +10,73 @@ public class Main {
 		
 		int numeroConta;
 		double saldo = 0.00;
-		double deposito;
-		double saque;
+		double deposito = 0.00;
+		double saque = 0.00;
 		String nomeTitular;
+		char decisao;
 		
 		System.out.println("Seja bem-vindo ao banco JVC!");
 		System.out.print("Digite o nome do titular: ");
 		nomeTitular = entrada.nextLine();	
-		System.out.print("Agora digite o número da conta: ");
+		System.out.print("Agora digite o número da conta de 5 dígitos: ");
 		numeroConta = entrada.nextInt();
+		if (numeroConta > 99999 || numeroConta < 10000) {
+			System.out.println("Digite uma conta válida!");
+			System.out.println("Exemplo: 1****");
+			System.out.println("Tente novamente: ");
+			numeroConta = entrada.nextInt();
+			
+		}
+			
 		System.out.println("Olá " + nomeTitular + ", seja bem-vindo a sua conta do banco JVC!");
 		System.out.println();
 		System.out.printf("Seu saldo atual é de: %.2f %n", saldo);
 		System.out.println();
-		System.out.println("Informe o valor do depósito inicial:");
-	    System.out.print("Quanto você quer depositar na sua conta? ");
-	    deposito = entrada.nextDouble();
-	    saldo = saldo + deposito;
-	    System.out.printf("Seu saldo atual é de: %.2f %n", saldo);
-	    System.out.println("Gostaria de fazer um saque? ");
+		System.out.print("Você gostaria de fazer um deposito? s/n  ");
+		decisao = entrada.next().charAt(0);
+		if (decisao == 's') {
+			System.out.print("Informe o valor do depósito inicial:");
+			deposito = entrada.nextDouble();
+		}
+		if(deposito < 0.00 ) { 
+			System.out.println("Você não pode depositar um valor negativo, tente novamente");
+			System.out.print("Informe o valor do depósito inicial:");
+			deposito = entrada.nextDouble();
+			saldo = saldo + deposito;
+			System.out.printf("Seu saldo atual é de: %.2f %n", saldo);	
+		}
+		else {
+			saldo = deposito;
+			System.out.printf("Seu saldo atual é de: %.2f %n", saldo);	
+		}
+		
+	  
+	    System.out.print("Gostaria de fazer um saque? s/n  ");
+	    decisao = entrada.next().charAt(0);
+	    if (decisao == 's') {
 	    System.out.print("Informe o valor do saque: ");
 	    saque = entrada.nextDouble();
-	    saldo = saldo - saque;
-	    System.out.printf("Seu saldo atual é de: %.2f %n", saldo);
-	    
+		   
+	    if ( saque < 0) {
+	    	System.out.println("Valor de saque inválido. Operação cancelada.");
+	    	
+			
+	    }
+	    else if (saque > saldo) {
+			System.out.println("Saldo insuficiente!");
+			}
+		else   {
+			saldo = saldo - saque;
+	    }  
+	    }
+	    System.out.println();
 	    System.out.println("Extrato da conta:");
 	    System.out.println("Conta: " + numeroConta);
 	    System.out.println("Nome: " + nomeTitular);
 	    System.out.printf("Saldo final: %.2f %n ", saldo);
 	    
 	    
-	   entrada.close(); 
+	    entrada.close(); 
 	}
 
 }
